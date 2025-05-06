0fff36336c135247dc7743c67e9c7119ab368493
gerysantoso03 <gerysantos03@gmail.com>
2025-05-06 18:31:12 +0700

    Update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/gerysantoso03/go-say-hello
+module github.com/gerysantoso03/go-say-hello/v2
 
 go 1.24
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,12 @@
 package go_say_hello
 
-func SayHello() string {
+/*
+	Major Update
+	- Biasanya kita merubah kode yg tidak backward compatibility, artinya org yg memanggil module kita akan terkena error yg signifikan.
+	- Salah satu caranya dengan merubah nama module, biasanya ditambah versi di belakang nama module sebelum.
+*/
+
+// Add parameter string to SayHello function (major update)
+func SayHello(name string) string {
 	return "Hello World!"
 }
